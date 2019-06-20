593aefec105c260729a7f8a45bcf5d56d76a633c
booydar <48096255+booydar@users.noreply.github.com>
2019-06-20 10:17:38 +0300

    Update Matrix.h

--- 03/Matrix.h
+++ 03/Matrix.h
@@ -3,64 +3,66 @@ using namespace std;
 
 [[noreturn]] void error() { throw invalid_argument("error"); }
 
+class Row
+{
+private:
+    int* sdata_;
+    int len;
+
+public:        
+    Row(const int l): len(l), sdata_(new int(l)){
+        for(int i=0; i<l; i++)
+            sdata_[i] = 0;
+    };          
+
+
+    int& operator[](size_t pos)
+    {
+        if(pos >= len)
+            throw out_of_range("");
+        return sdata_[pos];
+    }
+    
+    const int& operator[](size_t pos) const
+    {
+        if(pos >= len)
+            throw out_of_range("");
+        return sdata_[pos];
+    }
+    
+    Row& operator *= (int mult)
+    {
+        for(int i=0; i<len; i++)
+            sdata_[i] *= mult;
+    }
+    bool operator == (const Row& other)
+    {
+        for(int i=0; i<len; i++)
+            if(sdata_[i] != other.sdata_[i])
+                return false;
+        return true;
+    }
+    bool operator != (const Row& other)
+    {
+        return !(*this == other);
+    }       
+};
+
+
+
 class Matrix
 {
+private:
+    const size_t rows;
+    const size_t cols;
+    Row** data_;
+
 public:
-
-    class Row
-    {
-    public:
-        
-        Row(const int l): len(l), sdata_(new int(l)){
-            for(int i=0; i<l; i++)
-                sdata_[i] = 0;
-        };          
-        int* sdata_;
-        int len;
-    
-        int& operator[](size_t pos)
-        {
-            if(pos >= len)
-                throw out_of_range("");
-            return sdata_[pos];
-        }
-        
-        const int& operator[](size_t pos) const
-        {
-            if(pos >= len)
-                throw out_of_range("");
-            return sdata_[pos];
-        }
-        
-        Row& operator *= (int mult)
-        {
-            for(int i=0; i<len; i++)
-                sdata_[i] *= mult;
-        }
-        bool operator == (const Row& other)
-        {
-            for(int i=0; i<len; i++)
-                if(sdata_[i] != other.sdata_[i])
-                    return false;
-            return true;
-        }
-        bool operator != (const Row& other)
-        {
-            return !(*this == other);
-        }       
-    };
-
-
-
     Matrix(size_t r, size_t c)
         : rows(r), cols(c), data_(new Row*[r]){
             for(int i = 0; i < rows; i++)
                 data_[i] = new Row(c);
         };
-
-    const size_t rows;
-    const size_t cols;
-    Row** data_;
 
     Row operator[](int row)
     {

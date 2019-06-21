7cde9334c69c0ccdf25cb1947d41825de0283235
booydar <48096255+booydar@users.noreply.github.com>
2019-06-21 12:27:44 +0300

    Update Matrix.h

--- 03/Matrix.h
+++ 03/Matrix.h
@@ -10,11 +10,7 @@ private:
     int len;
 
 public:        
-    Row(const int l): len(l), sdata_(new int(l)){
-        for(int i=0; i<l; i++)
-            sdata_[i] = 0;
-    };          
-
+    Row(const int l, int* ptr): len(l), sdata_(ptr){};
 
     int& operator[](size_t pos)
     {
@@ -29,25 +25,7 @@ public:
             throw out_of_range("");
         return sdata_[pos];
     }
-    
-    Row& operator *= (int mult)
-    {
-        for(int i=0; i<len; i++)
-            sdata_[i] *= mult;
-    }
-    bool operator == (const Row& other)
-    {
-        for(int i=0; i<len; i++)
-            if(sdata_[i] != other.sdata_[i])
-                return false;
-        return true;
-    }
-    bool operator != (const Row& other)
-    {
-        return !(*this == other);
-    }       
 };
-
 
 
 class Matrix
@@ -55,38 +33,38 @@ class Matrix
 private:
     const size_t rows;
     const size_t cols;
-    Row** data_;
+    int** data_;
 
 public:
     Matrix(size_t r, size_t c)
-        : rows(r), cols(c), data_(new Row*[r]){
+        : rows(r), cols(c), data_(new int*[r]){
             for(int i = 0; i < rows; i++)
-                data_[i] = new Row(c);
+                data_[i] = new int[c];            
         };
 
     Row operator[](int row)
     {
         if(row >= rows)
             throw out_of_range("");
-        return *data_[row];
+        return Row(cols, data_[row]);
     }
     
     const Row operator[](int row) const
     {
         if(row >= rows)
             throw out_of_range("");
-        return *data_[row];
+        return Row(cols, data_[row]);
     }
     
     bool operator==(const Matrix& other) const      
     {
-        
         if(this == &other)
             return true;
         
         for(int i=0; i<rows; i++)
-            if(data_[i] != other.data_[i])
-                return false;
+            for(int j=0; j<cols; j++)
+                if(data_[i*cols + j] != other.data_[i*cols + j])
+                    return false;
         return true;            
     }
 
@@ -98,7 +76,8 @@ public:
     Matrix& operator*=(int mult)
     {
         for(int i=0; i<rows; i++)
-            *data_[i] *= mult;
+            for(int j=0; j<cols; j++)
+                data_[i][j] *= mult;
         return *this;
     }
 
